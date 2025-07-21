e083f802781e2e9f01585237e5dca9e4448db8c5
Landeremmanson <solomonemmanson491@gmail.com>
2025-07-21 01:38:27 +0100

    week3 project

---
+++ scripts/place.js
@@ -0,0 +1,29 @@
+window.onload = function () {
+
+    const currentYear = new Date().getFullYear();
+    document.getElementById("copyright").innerHTML =
+        `&copy; ${currentYear} 🌼 Solomon Lander 🌼 Akwa Ibom State, Nigeria`;
+
+
+    function updateDateTime() {
+        const now = new Date();
+        const formattedDate = `${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear()}`;
+        const formattedTime = `${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}`;
+        document.getElementById("lastModified").textContent = `Current Time: ${formattedDate} ${formattedTime}`;
+    }
+
+
+    updateDateTime();
+
+
+    setInterval(updateDateTime, 1000);
+
+
+    const menuButton = document.getElementById("menu");
+    const navMenu = document.getElementById("nav-menu");
+
+    menuButton.addEventListener("click", () => {
+        navMenu.classList.toggle("show");
+        menuButton.textContent = menuButton.textContent === "✖" ? "☰" : "✖";
+    });
+};
